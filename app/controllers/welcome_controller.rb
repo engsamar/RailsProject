3dcfb41add8b3179408749511924eaa6bd89ed4d
sherif <sherif.elkhiat.700@gmail.com>
2016-05-22 22:27:21 +0200

    picture

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,8 @@
 require 'simple_app/sse.rb' 
 class WelcomeController < ApplicationController
-  #include ActionController::Live
+  include ActionController::Live
   before_filter :authenticate_user!
-
+  # session[:n]=1
   def index
   	 @orders = Order.where(:user_id => current_user.id).limit(3).order("id desc")
   	 #select current user friends 
@@ -22,11 +22,14 @@ class WelcomeController < ApplicationController
     sse = SimpleApp::SSE.new(response.stream)
     begin
       20.times do
-       # messages = Message.where("created_at > ?", 3.seconds.ago)
-        invits = Invitation.where("user_id = ?",current_user.id)
-        unless invits.empty?
+       messages = Message.where("created_at > ?", 3.seconds.ago)
+        # invits = Invitation.where("user_id = ?",current_user.id)
+        unless messages.empty?
           # make your action here for notification
-           sse.write({invits: invits.as_json}, {event: 'refresh'})
+           # sse.write({messages: messages.as_json}, {event: 'refresh'})
+           # session[:notification] = "ssss"
+           # session[:n] += 1
+           # redirect_to '/welcome/index'
         end
         sleep 3
       end
@@ -35,6 +38,11 @@ class WelcomeController < ApplicationController
     ensure
       sse.close
     end
+  end
+
+
+  def notify
+
   end
 
 end
